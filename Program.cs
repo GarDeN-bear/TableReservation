bec1e1925158522a1ba62fd9201e9ca3f4835b51
GarDeN <garpinich22@mail.ru>
2023-06-28 18:11:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TableReservation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resetrvationIsOpen = true;
+            Random rnd = new Random();
+            int countTables = 5;
+            Table[] tables = new Table[countTables];
+            for (int i = 0; i < countTables; ++i)
+            {
+                tables[i] = new Table(i + 1, rnd.Next(2, 6));
+            }
+            while (resetrvationIsOpen)
+            {
+                for (int i = 0; i < countTables; ++i)
+                {
+                    tables[i].ShowInfo();
+                }
+                int reservationNumber = rnd.Next(1, 5);
+                int countPlaces = rnd.Next(2, 6);
+                tables[reservationNumber - 1].ReservePlaces(countPlaces);
+                while (true)
+                {
+                    Console.SetCursorPosition(0, 17);
+                    string clear = "                                                     ";
+                    Console.WriteLine(clear);
+                    Console.WriteLine(clear);
+                    Console.WriteLine(clear);
+                    Console.WriteLine(clear);
+                    Console.SetCursorPosition(0, 17);
+                    Console.Write($"Остановить регистрацию?)\nВведите [д/н]: ");
+                    string breakReservation = Console.ReadLine();
+                    if (breakReservation == "д")
+                    {
+                        resetrvationIsOpen = false;
+                        break;
+                    }
+                    else if (breakReservation != "н" && breakReservation != "д")
+                    {
+                        Console.Write("Я не знаю такой команды.\nНажмите на любую кнопку для повтора команды");
+                        Console.ReadKey();
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.Clear();
+            }
+
+        }
+    }
+}
---
+++ Table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableReservation
+{
+    internal class Table
+    {
+        private int Number;
+        private int MaxFreePlaces;
+        public Table (int number, int maxFreePlaces)
+        {
+            Number = number;
+            MaxFreePlaces = maxFreePlaces;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Номер стола: {Number}\nСвободных мест за столом: {MaxFreePlaces}");
+        }
+        public void ReservePlaces(int countPlaces)
+        {
+            Console.SetCursorPosition(0, 15);
+            Console.WriteLine($"Необходимо забронировать {countPlaces} мест за столом под номером {Number}");
+            if (countPlaces > MaxFreePlaces)
+            {
+                Console.WriteLine($"За столом под номером {Number} недостаточно свободных мест");
+            }
+            else
+            {
+                MaxFreePlaces -= countPlaces;
+                Console.WriteLine($"Бронирование прошло успешно");
+            }
+        }
+
+    }
+}
